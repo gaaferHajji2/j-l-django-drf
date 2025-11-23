4bf130bee07e161c917d1d0697ffc0e175232c38
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-23 13:28:13 +0300

    Remove unused spaces

--- store/models.py
+++ store/models.py
@@ -16,22 +16,18 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255, unique=True)
-
     featured_product = models.ForeignKey(
         to='Product', on_delete=models.SET_NULL, null=True, related_name='+'
     )
 
     def __str__(self):
         return self.title
-    
     class Meta:
         ordering = ['title']
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-
     slug  = models.SlugField()
-
     description = models.TextField(null=True, blank=True)
     unit_price = models.DecimalField(
         max_digits=6, 
@@ -42,26 +38,20 @@ class Product(models.Model):
     )
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-
     collection = models.ForeignKey(to=Collection, on_delete=models.PROTECT, related_name='products')
-
     # promotions = models.ManyToManyField(to=Promotion, related_name='products')
     promotions = models.ManyToManyField(to=Promotion, blank=True)
 
     def __str__(self):
         return str(self.id) + ' - ' + self.title # type: ignore
-    
     class Meta:
         ordering = ['title']
 
 class Customer(models.Model):
 
     MEMBERSHIP_BRONZE = 'B'
-
     MEMBERSHIP_SLIVER = 'S'
-
     MEMBERSHIP_GOLD = 'G'
-
     MEMBERSHIP_CHOICES = [
         (MEMBERSHIP_BRONZE, 'Bronze'),
         (MEMBERSHIP_SLIVER, 'Silver'),
@@ -78,7 +68,6 @@ class Customer(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.first_name} {self.user.last_name}"
-
     class Meta:
         ordering = ['user__first_name', 'user__last_name']
         permissions = [
@@ -88,7 +77,6 @@ class Customer(models.Model):
     @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name # type: ignore
-    
     @admin.display(ordering='user__last_name')
     def last_name(self):
         return self.user.last_name; # type: ignore
@@ -97,7 +85,6 @@ class Order(models.Model):
     PAYMENT_STATUS_PENDING  = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
     PAYMENT_STATUS_FAILED   = 'F'
-
     PAYMENT_STATUS_CHOICES = [
         (PAYMENT_STATUS_PENDING, 'Pending'),
         (PAYMENT_STATUS_COMPLETE, 'Complete'),
@@ -105,11 +92,8 @@ class Order(models.Model):
     ]
 
     placed_at = models.DateTimeField(auto_now_add=True)
-
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-
     customer = models.ForeignKey(to=Customer, on_delete=models.PROTECT)
-
     class Meta:
         permissions= [
             ('cancel_order', 'can cancel order')
@@ -124,7 +108,6 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city   = models.CharField(max_length=255)
-
     customer = models.OneToOneField(to=Customer, on_delete=models.CASCADE, primary_key=True)
 
 class Cart(models.Model):
@@ -134,7 +117,6 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE, related_name='items', default=None)
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
-
     quantity = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(1)]
     )
