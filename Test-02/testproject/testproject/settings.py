3a13f485220c9be0d32e9f1685f83dac0de2645c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-03 15:07:04 +0300

    Install mysqlclient & migrate & init watchlist app

--- Test-02/testproject/testproject/settings.py
+++ Test-02/testproject/testproject/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Added By Jafar Loka
+    'rest_framework',
+    'test_01',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +78,11 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'NAME': 'test'
     }
 }
 
