c3723fa7e0d4f813f3251927ba22758a2b511853
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-16 12:44:33 +0300

    build post details view

--- Test-02/jlokastite/blog/views.py
+++ Test-02/jlokastite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 from .models import Post
 
 # Create your views here.
@@ -6,3 +7,13 @@ def post_list(request):
     posts = Post.published.all()
     # in this way we avoid positional arguments error
     return render(request, 'blog/post/list.html', {'posts': posts});
+
+def post_detail(request, id: int):
+    try:
+        # post = Post.publish.get(pk=id)
+        post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    except Post.DoesNotExist:
+        raise Http404("No post found")
+    return render(
+        request, 'blog/post/detail.html', {'post': post}
+    )
