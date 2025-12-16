8ea32ea9165afa469492bf74f75adc8588101ebc
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-16 12:39:32 +0300

    build post list view

--- Test-02/jlokastite/blog/views.py
+++ Test-02/jlokastite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    # in this way we avoid positional arguments error
+    return render(request, 'blog/post/list.html', {'posts': posts});
