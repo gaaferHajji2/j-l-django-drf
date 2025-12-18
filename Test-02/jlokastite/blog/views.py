dd62c248cc20790bdb72acbd4b429e2a632c55b1
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-18 14:38:50 +0300

    Adding pagination to views

--- Test-02/jlokastite/blog/views.py
+++ Test-02/jlokastite/blog/views.py
@@ -1,10 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 # from django.http import Http404
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1);
+    posts = paginator.page(page_number)
     # in this way we avoid positional arguments error
     return render(request, 'blog/post/list.html', {'posts': posts});
 
