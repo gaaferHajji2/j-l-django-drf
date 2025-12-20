2b8c46b957cc79ae5277564e177b84d9f1b551ee
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-20 19:45:06 +0300

    Handle pagination errors

--- Test-02/jlokastite/blog/views.py
+++ Test-02/jlokastite/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 # from django.http import Http404
 from .models import Post
@@ -9,7 +9,12 @@ def post_list(request):
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1);
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(paginator.num_pages)
     # in this way we avoid positional arguments error
     return render(request, 'blog/post/list.html', {'posts': posts});
 
