41b72585d467caffcfe32af79b09cb48b88bb013
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-27 12:54:26 +0300

    Creating comment model & migration

--- Test-02/jlokastite/blog/models.py
+++ Test-02/jlokastite/blog/models.py
@@ -46,3 +46,19 @@ class Post(models.Model):
             self.publish.day,
             self.slug,
         ])
+
+class Comment(models.Model):
+    post = models.ForeignKey(to=Post, on_delete= models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=180)
+    email = models.EmailField()
+    body  = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes  = [models.Index(fields=['created'])]
+    
+    def __str__(self):
+        return f'Comment by {self.name} by {self.post}'
