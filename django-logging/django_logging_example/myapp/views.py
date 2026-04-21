c3af15caac67fe2b0a2c2e82484bc44f3d19668d
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-21 16:59:28 +0300

    Create a simple api view class

--- django-logging/django_logging_example/myapp/views.py
+++ django-logging/django_logging_example/myapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import logging
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+class MyView(APIView):
+    def get(self, request):
+        logger.debug("Debug message: This will only show in DEBUG mode")
+        logger.info("Info: User %s accessed the homepage", request.user.username)
+        logger.warning("Warning: Something suspicious happened")
+        
+        try:
+            # Simulate an error
+            1 / 0
+        except Exception as e:
+            logger.error("Error occurred in MyView.get()", exc_info=True)
+            logger.exception("This also logs the full traceback")  # Same as above but cleaner
+
+        return Response("Hello, logging world!")
