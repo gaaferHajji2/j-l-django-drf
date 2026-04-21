ae3d769bfdde572db30de2c45a2af1fc7c81f2b3
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-21 16:59:11 +0300

    Create a model with migration

--- django-logging/django_logging_example/myapp/models.py
+++ django-logging/django_logging_example/myapp/models.py
@@ -1,3 +1,14 @@
+import logging
 from django.db import models
 
+logger = logging.getLogger(__name__)
 # Create your models here.
+class MyModel(models.Model):
+    name = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        logger.info("Saving MyModel instance: %s", self.name)
+        super().save(*args, **kwargs)
+        
+        if self.name == "bad":
+            logger.error("Invalid name detected: %s", self.name)
