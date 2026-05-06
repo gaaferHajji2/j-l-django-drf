8d08eb5e6e1f3b12f8a0e3606fc1358897bbea06
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-06 03:22:35 +0300

    Remove unused lines

--- Test-01/store/urls.py
+++ Test-01/store/urls.py
@@ -1,11 +1,4 @@
-# from django.urls import path
-
-# from rest_framework.routers import SimpleRouter
-
 from rest_framework_nested import routers
-
-# from pprint import pprint
-
 from . import views
 
 router = routers.DefaultRouter() # type: ignore
@@ -22,18 +15,5 @@ products_router.register('images', views.ProductImageViewSet, basename='product-
 
 carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
 carts_router.register('items', views.CartItemViewSet, basename='cart-items')
-
-# pprint(router.urls)
-
-# urlpatterns = [
-    # path('product/', views.ProductList.as_view()),
-    # path('product/<int:pk>/', views.ProductDetail.as_view()),
-
-    # path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
-
-    # path('collection/', views.CollectionList.as_view()),
-
-    # path('collection/<int:pk>/', views.CollectionDetail.as_view())
-# ];
 
 urlpatterns = router.urls + products_router.urls + carts_router.urls
